f3a865bb6067efb48ac9cafc66e7e69d7a22ca8c
Alper Akgün <alper.akgun@sayarus.com>
2011-05-19 11:13:36 +0300

    testing against facebook graph api

--- test/base.rb
+++ test/base.rb
@@ -4,14 +4,15 @@ require './lib/esnek'
 require 'minitest/autorun'
 
 describe Esnek do
-  before do
-    @esnek=Esnek.new
+  before do      
   end
 
-  describe "When an esnek against public APIs is given" do
-    it "should return call a .." do
-      @esnek.get
+  describe "When I query Facebook graph API to count the # of shares of url" do
+    it "should return an object which responds to :shares" do
+      fb = Esnek.new('http://graph.facebook.com')
+      res = fb.send(:"http://www.wsirussia.ru").get
+      assert res.respond_to?(:shares)
     end
   end
-  
+
 end
