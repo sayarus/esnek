9c5ac786029af03ffc250db46e388f8401e67bcd
Alper Akgun <alper.akgun@sayarus.com>
2012-07-08 22:02:31 +0400

    oauth support

--- lib/esnek/base.rb
+++ lib/esnek/base.rb
@@ -6,11 +6,16 @@ require 'ostruct'
 # highly-available, real time search RESTful search engine communicating by JSON over HTTP, based on _Lucene_ (http://lucene.apache.org). 
 class Esnek
   attr_accessor :chain, :url_root
-  def initialize(url_root,json_api=true,headers={})
+  def initialize(url_root,options={:json_api=>true,:headers={}})
     @url_root = url_root
     @chain = []
-    @json_api = json_api
-    @headers=headers
+    @json_api = options[:json_api]
+    @headers= options[:headers]
+    if options[:oauth] # Esnek assumes that oauth ruby gem is installed
+      options[:oauth][:scheme] ||= :header
+      consumer = OAuth::Consumer.new(options[:oauth][:consumer_key],options[:oauth][:consumer_secret], {:site => options[:oauth][:site], :scheme => options[:oauth][:scheme]})
+      @access_token = OAuth::AccessToken.from_hash(consumer, {:oauth_token => options[:oauth][:oauth_token], :oauth_token_secret => options[:oauth][:oauth_token_secret]} )
+    end
   end
 
   def parse_json(resp)
@@ -34,9 +39,19 @@ class Esnek
       @chain = []
       headers = {:params => params}.merge!(@headers)
       data = block_given? ? block.call : nil rescue nil
+
+      # if a JSON api is set in initializer both the payload data and the result will be un/jsonized
       if @json_api
         headers.merge!({:content_type => :json, :accept => :json})
         data = data.to_json if data rescue nil
+      end
+      # if a oauth token exist, use it; unfortunately restclient does not allow a proper
+      if @access_token
+        access_token=@access_token
+        RestClient.reset_before_execution_procs
+        RestClient.add_before_execution_proc do |req, params|
+          access_token.sign! req
+        end
       end
       resp =  if [:put, :post,:patch].include?(method_sym)
                 RestClient.send(method_sym, url, data, headers)
